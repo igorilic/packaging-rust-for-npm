c8a95265166b73545752f02dcfaa52a24718cd70
Igor Ilic <ilicigor@gmail.com>
2024-01-30 23:38:03 +0100

    changed name to test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use inquire::InquireError;
+use inquire::Select;
+
 fn main() {
-    println!("Hello, world!");
+    let options: Vec<&str> = vec![
+        "Banana",
+        "Apple",
+        "Strawberry",
+        "Grapes",
+        "Lemon",
+        "Tangerine",
+        "Watermelon",
+        "Orange",
+        "Pear",
+        "Avocado",
+        "Pineapple",
+    ];
+
+    let ans: Result<&str, InquireError> =
+        Select::new("What's your favorite fruit?", options).prompt();
+
+    match ans {
+        Ok(choice) => println!("{}! That's mine too!", choice),
+        Err(_) => println!("There was an error, please try again"),
+    }
 }
